8d0353f5798f5a4ab438362a746614121df14441
Naruto777549649 <sikrar026@gmail.com>
2025-04-29 01:33:53 +0530

    Update db.py

--- bot/db.py
+++ bot/db.py
@@ -1,7 +1,7 @@
 from pymongo import MongoClient
 
 # MongoDB URL (Replace with your connection string)
-MONGO_URL = "mongodb+srv://Naruto:Naruto777549@cluster1.ge8jrgm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1"
+MONGO_URL = "mongodb+srv://Naruto:Naruto7775549@cluster1.ge8jrgm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1"
 
 # Create a client
 client = MongoClient(MONGO_URL)
