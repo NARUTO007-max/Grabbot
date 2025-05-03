c7955a86556e1e653bf030ae73ce858df556f0a2
Naruto777549649 <sikrar026@gmail.com>
2025-05-03 07:23:56 +0530

    Update __init__.py

--- bot/__init__.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'your_secret_key_here'
-
-# Import routes after app is initialized
-from bot.routes import main
-app.register_blueprint(main)
