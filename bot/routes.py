f4b8a7ced79ba2a3c7a1792d1d3ed0913240aae9
Naruto777549649 <sikrar026@gmail.com>
2025-05-07 03:10:15 +0530

    Update routes.py

---
+++ bot/routes.py
@@ -0,0 +1,31 @@
+from AB import bot 
+from pyrogram import Client, filters 
+import sys 
+import os 
+import requests 
+import logging
+import AB 
+
+
+_DEV_ = 6239769036 
+
+@bot.on_message(filters.command(["logs", "log"]) & filters.user(_DEV_))
+async def logs(_, message):
+    run_logs = run("tail log.txt")
+    text = await message.reply_text("`Getting logs...`")
+    await bot.send_chat_action(message.chat.id, ChatAction.TYPING)
+    await message.reply_text(f"```shell\n{run_logs}```")
+    await text.delete()
+
+
+@bot.on_message(filters.command(["flogs", "flog"]) & filters.user(_DEV_))
+async def logs(_, message):
+    run_logs = run("cat log.txt")
+    text = await message.reply_text("`Sending Full logs...`")
+    await bot.send_chat_action(message.chat.id, ChatAction.UPLOAD_DOCUMENT)
+    with io.BytesIO(str.encode(run_logs)) as logs:
+        logs.name = "log.txt"
+        await message.reply_document(
+            document=logs,
+        )
+    await text.delete()
