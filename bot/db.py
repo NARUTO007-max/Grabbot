bed2e550f582be5c7b2732626d2fc7cc9deb44d6
Naruto777549649 <sikrar026@gmail.com>
2025-05-20 04:48:09 +0530

    Update db.py

--- bot/db.py
+++ bot/db.py
@@ -1,27 +1,9 @@
-# db.py
-from pymongo import MongoClient
-
-client = MongoClient("mongodb://localhost:27017/")
-db = client["anime_multiverse"]
-users = db["users"]
-
-def get_or_create_user(user_id, username):
-    user = users.find_one({"user_id": user_id})
-    if not user:
-        user = {
-            "user_id": user_id,
-            "username": username,
-            "selected_character": None,
-            "level": 1,
-            "gold": 0,
-            "diamonds": 0,
-            "current_universe": None,
-        }
-        users.insert_one(user)
-    return user
-
-def set_character(user_id, character_name):
-    users.update_one(
+def add_waifu_to_user(user_id, username, waifu_data):
+    waifu_col.update_one(
         {"user_id": user_id},
-        {"$set": {"selected_character": character_name}}
+        {
+            "$set": {"username": username},
+            "$push": {"waifus": waifu_data}
+        },
+        upsert=True
     )
